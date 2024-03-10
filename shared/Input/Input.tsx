515c5b64b734d3e37cf9d9d34ab184a4dbd2d94c
Raman Radzivonau <radzivonau.raman@gmail.com>
2024-03-10 16:59:58 +0300

    Added input component

---
+++ shared/Input/Input.tsx
@@ -0,0 +1,58 @@
+import { FC, useState } from "react";
+import {
+  TextInput,
+  TextInputProps,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { Colors } from "../tokens";
+import EyeOpenedIconSvg from "../../assets/icons/EyeOpenedIconSvg";
+import EyeClosedIconSvg from "../../assets/icons/EyeClosedIconSvg";
+
+interface InputProps extends TextInputProps {
+  type?: "text" | "password";
+}
+
+export const Input: FC<InputProps> = ({ type = "text", ...props }) => {
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
+  return (
+    <View>
+      <TextInput
+        style={[Styles.input, type === "password" && { paddingRight: 60 }]}
+        placeholderTextColor={Colors.gray}
+        secureTextEntry={type === "password" && !isPasswordVisible}
+        cursorColor={Colors.primary}
+        {...props}
+      />
+      {type === "password" && (
+        <TouchableOpacity
+          onPress={() => setIsPasswordVisible((state) => !state)}
+          style={Styles.icon}
+        >
+          {isPasswordVisible ? <EyeOpenedIconSvg /> : <EyeClosedIconSvg />}
+        </TouchableOpacity>
+      )}
+    </View>
+  );
+};
+
+const Styles = StyleSheet.create({
+  input: {
+    height: 58,
+    paddingHorizontal: 24,
+    borderRadius: 10,
+    backgroundColor: Colors.violetDark,
+    // fontFamily: 'Fira Sans',
+    fontSize: 16,
+    lineHeight: 19,
+    color: Colors.gray,
+  },
+  icon: {
+    position: "absolute",
+    top: "50%",
+    right: 20,
+    transform: [{ translateY: -12 }],
+  },
+});
---
+++ shared/tokens.ts
@@ -0,0 +1,18 @@
+export const Colors = {
+    black: '#16171D',
+    gray: '#AFB2BF',
+    link: '#A97BFF',
+    primary: '#6C38CC',
+    primaryHover: '#452481',
+    violetDark: '#2E2D3D',
+    white: '#FAFAFA'
+}
+
+export const Gaps = {
+    g16: 16,
+    g50: 50
+}
+
+export const Radius = {
+    r10: 10
+}
