f2fbe6b5a5635e0098737a11e840ab57090784f3
Raman Radzivonau <radzivonau.raman@gmail.com>
2024-03-10 17:23:32 +0300

    Updated login screen

--- App.tsx
+++ App.tsx
@@ -1,39 +1,21 @@
-import { StyleSheet, Text, View, TextInput, Pressable } from "react-native";
+import { StyleSheet, Text, View, Pressable, Image } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { Colors, Gaps, Fonts, Input, Button } from "./shared";
+import LogoSvg from "./assets/LoginScreen/LogoSvg";
 
 export default function App() {
   return (
     <SafeAreaView style={Styles.screen}>
       <StatusBar style="light" />
       <View style={Styles.container}>
-        <Text
-          style={{
-            fontSize: 18,
-            lineHeight: 22,
-            fontWeight: "600",
-            textAlign: "center",
-            color: "#FAFAFA",
-          }}
-        >
-          PurpleSchool
-        </Text>
+        <LogoSvg />
         <View style={Styles.form}>
-          <TextInput
-            placeholder="Email"
-            style={Styles.input}
-            placeholderTextColor="#AFB2BF"
-          />
-          <TextInput
-            placeholder="Пароль"
-            style={Styles.input}
-            placeholderTextColor="#AFB2BF"
-          />
-          <Pressable style={Styles.logInButton}>
-            <Text style={Styles.logInButtonText}>Войти</Text>
-          </Pressable>
+          <Input placeholder="Email" />
+          <Input type="password" placeholder="Пароль" />
+          <Button text="Войти" />
         </View>
-        <Text style={Styles.resetButton}>Восстановить пароль</Text>
+        <Text style={Styles.resetLink}>Восстановить пароль</Text>
       </View>
     </SafeAreaView>
   );
@@ -42,46 +24,27 @@ export default function App() {
 const Styles = StyleSheet.create({
   screen: {
     flex: 1,
-    backgroundColor: "#16171D",
+    backgroundColor: Colors.black,
   },
   container: {
     flex: 1,
     paddingHorizontal: 55,
     justifyContent: "center",
-    gap: 50,
+    alignItems: "center",
+    gap: Gaps.g50,
+  },
+  logo: {
+    width: 163,
   },
   form: {
-    gap: 16,
+    alignSelf: "stretch",
+    gap: Gaps.g16,
   },
-  input: {
-    height: 58,
-    paddingLeft: 26,
-    paddingRight: 24,
-    borderRadius: 10,
-    backgroundColor: "#2E2D3D",
-    // fontFamily: 'Fira Sans',
-    fontSize: 16,
-    lineHeight: 19,
-    color: "#AFB2BF",
-  },
-  logInButton: {
-    justifyContent: "center",
-    alignItems: "center",
-    height: 58,
-    borderRadius: 10,
-    backgroundColor: "#6C38CC",
-  },
-  logInButtonText: {
-    // fontFamily: 'Fira Sans',
-    fontSize: 18,
-    lineHeight: 22,
-    color: "#FAFAFA",
-  },
-  resetButton: {
+  resetLink: {
     // fontFamily: "Fira Sans",
-    fontSize: 18,
-    lineHeight: 22,
+    fontSize: Fonts.f18,
+    lineHeight: 1.2,
     textAlign: "center",
-    color: "#A97BFF",
+    color: Colors.link,
   },
 });
